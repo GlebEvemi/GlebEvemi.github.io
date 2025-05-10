7540ae4da68617c2905bdb3c35f65f795ef5c835
GlebEvemi <gleb2466@gmail.com>
2025-05-10 15:16:54 +0300

    fix links

--- CALC/script.js
+++ CALC/script.js
@@ -6,7 +6,7 @@ const inputEur = document.querySelector('#eur'),
     inputEur.addEventListener('input', () => {
         const request = new XMLHttpRequest();
 
-        request.open('GET', 'js/current.json');
+        request.open('GET', 'current.json');
         request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
         request.send();
 
